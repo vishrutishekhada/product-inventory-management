4fdddee4c1e3dc00fe1f2946f4ffca101550bfa1
manasvipadaliya120848-blip <manasvipadaliya.120848@marwadiuniversity.ac.in>
2026-01-30 20:44:47 +0530

    Add files via upload

---
+++ UserSeeder.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use App\Models\Role;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    public function run()
+    {
+        $adminRole = Role::where('name', 'Admin')->first();
+        $managerRole = Role::where('name', 'Manager')->first();
+        $staffRole = Role::where('name', 'Staff')->first();
+
+        User::create([
+            'name' => 'Admin User',
+            'email' => 'admin@example.com',
+            'password' => Hash::make('password'),
+            'role_id' => $adminRole->id,
+        ]);
+
+        User::create([
+            'name' => 'Manager User',
+            'email' => 'manager@example.com',
+            'password' => Hash::make('password'),
+            'role_id' => $managerRole->id,
+        ]);
+
+        User::create([
+            'name' => 'Staff User',
+            'email' => 'staff@example.com',
+            'password' => Hash::make('password'),
+            'role_id' => $staffRole->id,
+        ]);
+    }
+}
